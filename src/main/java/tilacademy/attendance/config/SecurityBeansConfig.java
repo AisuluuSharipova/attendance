2f1b5d13b6bbc71533d2e8895a567fe58539cd9c
nurem1r <nuremir.sulaimanov@alatoo.edu.kg>
2025-11-28 15:22:24 +0600

    feat: add SecurityConfig beans configuration for security

---
+++ src/main/java/tilacademy/attendance/config/SecurityBeansConfig.java
@@ -0,0 +1,24 @@
+package tilacademy.attendance.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SecurityBeansConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(10);
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider(UserDetailsService uds, PasswordEncoder encoder) {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider(uds);
+        provider.setPasswordEncoder(encoder);
+        return provider;
+    }
+}
